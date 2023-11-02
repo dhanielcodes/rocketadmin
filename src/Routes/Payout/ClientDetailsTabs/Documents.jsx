eaa49a1a18d9fb3b5afb0f825d1bfb5929e7a62a
Daniel Adekoya <adekoyadaniel53@gmail.com>
2023-11-02 17:35:47 +0100

    chore:  client fund request log

--- src/Routes/Payout/ClientDetailsTabs/Documents.jsx
+++ src/Routes/Payout/ClientDetailsTabs/Documents.jsx
@@ -479,6 +479,204 @@ export default function Documents({ clientDetails, refetch }) {
               </div>
             </div>
           </div>
+
+          {/*  <div
+            style={{
+              display: "flex",
+              borderTop: "1px solid #EAECF0",
+              borderBottom: "1px solid #EAECF0",
+              paddingTop: "20px",
+              marginTop: "20px",
+              paddingBottom: "20px",
+              width: "100%",
+            }}
+          >
+            <div
+              style={{
+                padding: "14px",
+                border: "1px dashed #c1c1c1",
+                borderRadius: "10px",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <DownloadIcon
+                  style={{
+                    marginRight: "10px",
+                  }}
+                />
+
+                <div
+                  onClick={() => {
+                    if (
+                      clientDetails?.companyCertificateURL?.includes("https")
+                    ) {
+                      setModal2(true);
+                      setType("view");
+                      setImage(clientDetails?.companyCertificateURL);
+                    } else {
+                      toast.error("Invalid file type, please edit this file");
+                    }
+                  }}
+                  style={{
+                    width: "100%",
+                    wordBreak: "break-all",
+                    color: "#3769b9",
+                    cursor: "pointer",
+                  }}
+                >
+                  Company Certificate
+                </div>
+              </div>
+
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <TiPencil
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    setModal(true);
+                    setFile("companyCertificateURL");
+                  }}
+                />
+                <SmallDownload
+                  onClick={() => {
+                    downloadImage(
+                      clientDetails?.companyCertificateURL,
+                      "utility-bill.png"
+                    );
+                  }}
+                  style={{
+                    marginLeft: "14px",
+                    marginRight: "14px",
+                    cursor: "pointer",
+                  }}
+                />
+                <MDeleteIcon
+                  onClick={() => {
+                    setType();
+
+                    setModal2(true);
+                    setImage(clientDetails?.companyCertificateURL);
+                  }}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              borderTop: "1px solid #EAECF0",
+              borderBottom: "1px solid #EAECF0",
+              paddingTop: "20px",
+              marginTop: "20px",
+              paddingBottom: "20px",
+              width: "100%",
+            }}
+          >
+            <div
+              style={{
+                padding: "14px",
+                border: "1px dashed #c1c1c1",
+                borderRadius: "10px",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <DownloadIcon
+                  style={{
+                    marginRight: "10px",
+                  }}
+                />
+
+                <div
+                  onClick={() => {
+                    if (
+                      clientDetails?.articlesAndMemorandumOfAssociation?.includes(
+                        "https"
+                      )
+                    ) {
+                      setModal2(true);
+                      setType("view");
+                      setImage(
+                        clientDetails?.articlesAndMemorandumOfAssociation
+                      );
+                    } else {
+                      toast.error("Invalid file type, please edit this file");
+                    }
+                  }}
+                  style={{
+                    width: "100%",
+                    wordBreak: "break-all",
+                    color: "#3769b9",
+                    cursor: "pointer",
+                  }}
+                >
+                  Articles and Memorandum of Association
+                </div>
+              </div>
+
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <TiPencil
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    setModal(true);
+                    setFile("articlesAndMemorandumOfAssociation");
+                  }}
+                />
+                <SmallDownload
+                  onClick={() => {
+                    downloadImage(
+                      clientDetails?.articlesAndMemorandumOfAssociation,
+                      "utility-bill.png"
+                    );
+                  }}
+                  style={{
+                    marginLeft: "14px",
+                    marginRight: "14px",
+                    cursor: "pointer",
+                  }}
+                />
+                <MDeleteIcon
+                  onClick={() => {
+                    setType();
+
+                    setModal2(true);
+                    setImage(clientDetails?.articlesAndMemorandumOfAssociation);
+                  }}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
